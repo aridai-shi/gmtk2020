27a4bf4e438b0b34b19ec13223d170bc288f5a24
aridai <jakubkaczma5@gmail.com>
2020-07-12 12:01:03 +0200

    Gave Chestie *mortality*
    Aka the no-shell variant can perish now

--- Chestie/Chestie.gd
+++ Chestie/Chestie.gd
@@ -1,6 +1,7 @@
 extends KinematicBody2D
 var velocity = Vector2.ZERO
 var killpos = Vector2.ZERO
+var dead = false
 export (bool) var Shelled = false
 func _ready():
 	killpos.y = $KillArea.position.y
@@ -14,14 +15,22 @@ func _physics_process(delta):
 		$KillArea.scale.y = 1.8
 		$KillArea.position.y = killpos.y - 20
 		$BounceArea/CollisionShape2D.disabled = true
-	else:
-		$AnimatedSprite.play("no-shell")
 	if is_on_wall() && !($WallTimer.time_left>0):
 		$AnimatedSprite.flip_h = !$AnimatedSprite.flip_h
 		$WallTimer.start(0.5)
-	velocity.y += 650 * delta
-	velocity = move_and_slide(velocity, Vector2(0, -1))
-			
+	if dead:
+		if $AnimatedSprite.animation != "death":
+			$AnimatedSprite.play("death")
+		$BounceArea/CollisionShape2D.disabled = true
+		$KillArea/CollisionShape2D.disabled = true
+		$CollisionShape2D.disabled = true
+		if $AnimatedSprite.frame == 4:
+			queue_free()
+	else:
+		velocity.y += 650 * delta
+		velocity = move_and_slide(velocity, Vector2(0, -1))
+		if !Shelled:
+			$AnimatedSprite.play("no-shell")
 
 
 func _on_KillArea_body_entered(body):
@@ -33,3 +42,4 @@ func _on_KillArea_body_entered(body):
 func _on_BounceArea_body_entered(body):
 	if "Player" in body.name:
 		body.velocity.y = -300
+		dead = true
